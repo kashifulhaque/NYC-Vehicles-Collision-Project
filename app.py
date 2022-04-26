e7c55835ce7683e0b1e0c31c161a93fabf930c55
kashifulhaque <haque.kashiful7@gmail.com>
2022-04-26 17:07:31 +0530

    Added a map visualization

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
+import plotly.express as px
 
 # Add the data URL
 DATA_URL = (
@@ -30,6 +32,43 @@ data = load_data(100_000)
 st.header('Where are the most people injured in NYC?')
 injured_people = st.slider('Number of people injured', 0, 19)
 st.map(data.query('injured_persons >= @injured_people')[['latitude', 'longitude']].dropna(how='any'))
+
+st.header('Hw many collisions occur during a given time period of a day?')
+hour = st.sidebar.slider('Hours to look at', 0, 23)
+data = data[data['date/time'].dt.hour == hour]
+
+st.markdown('Vehicle collisions between %i:00 and %i:00' % (hour % 24, (hour + 1) % 24))
+midpoint = (np.average(data['latitude']), np.average(data['longitude']))
+st.write(pdk.Deck(
+  map_style='mapbox://styles/mapbox/light-v9',
+  initial_view_state={
+    'latitude': midpoint[0],
+    'longitude': midpoint[1],
+    'zoom': 11,
+    'pitch': 50,
+  },
+  layers=[
+    pdk.Layer(
+      'HexagonLayer',
+      data=data[['date/time', 'latitude', 'longitude']],
+      get_position=['longitude', 'latitude'],
+      radius=100,
+      extruded=True,
+      pickable=True,
+      elevation_scale=4,
+      elevation_range=[0, 1000],
+    ),
+  ],
+))
+
+st.subheader('Breakdown by minute between %i:00 and %i:00' % (hour % 24, (hour + 1) % 24))
+filtered = data[
+  (data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour + 1))
+]
+hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0, 60))[0]
+chart_data = pd.DataFrame({'minute': range(60), 'crashes': hist})
+fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
+st.write(fig)
 
 if st.checkbox('Show Raw Data 📝', False):
   st.subheader('Raw Data')
